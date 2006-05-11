ed39d93aef7816e639795146156aeddbc1ee2666
skilvington <skilvington@79acd270-d80d-0410-8ebe-ae0fbb1cb39d>
2006-05-11 11:40:42 +0000

    add MPEG stream type constants
    git-svn-id: https://redbutton.svn.sourceforge.net/svnroot/redbutton/redbutton-browser/trunk@53 79acd270-d80d-0410-8ebe-ae0fbb1cb39d

--- mpegts.h
+++ mpegts.h
@@ -5,6 +5,17 @@
 #ifndef __MPEGTS_H__
 #define __MPEGTS_H__
 
+/* stream types we care about */
+#define STREAM_TYPE_VIDEO_MPEG1     0x01
+#define STREAM_TYPE_VIDEO_MPEG2     0x02
+#define STREAM_TYPE_AUDIO_MPEG1     0x03
+#define STREAM_TYPE_AUDIO_MPEG2     0x04
+#define STREAM_TYPE_AUDIO_AAC       0x0f
+#define STREAM_TYPE_VIDEO_MPEG4     0x10
+#define STREAM_TYPE_VIDEO_H264      0x1b
+#define STREAM_TYPE_AUDIO_AC3       0x81
+#define STREAM_TYPE_AUDIO_DTS       0x8a
+
 typedef struct MpegTSContext MpegTSContext;
 
 MpegTSContext *mpegts_open(FILE *);
