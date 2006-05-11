1cfa4a3a3c3f0c125434ba33eed1a879466fc3e3
skilvington <skilvington@79acd270-d80d-0410-8ebe-ae0fbb1cb39d>
2006-05-11 08:27:18 +0000

    always set PES filter type to DMX_PES_OTHER
    git-svn-id: https://redbutton.svn.sourceforge.net/svnroot/redbutton/redbutton-download/trunk@49 79acd270-d80d-0410-8ebe-ae0fbb1cb39d

--- stream.c
+++ stream.c
@@ -28,11 +28,22 @@ add_demux_filter(char *demux_dev, uint16_t pid, dmx_pes_type_t pes_type)
 	pes_filter.output = DMX_OUT_TS_TAP;
 	pes_filter.flags = DMX_IMMEDIATE_START;
 	pes_filter.pid = pid;
+	/*
+	 * I think the way pes_type works is like this:
+	 * if you set it to DMX_PES_VIDEO then the output goes to the dvr0 device (as a TS)
+	 * but it also ends up on the video0 device (presumably as decoded video frames)
+	 * similarly for DMX_PES_AUDIO
+	 * if we set the pes_type to DMX_PES_OTHER then the output only goes to the dvr0 device
+	 * on my Nova-T card, there is no hardware MPEG decoder so setting the pes_type to DMX_PES_VIDEO returns an error (sometimes!)
+	 * anyway, always setting it to DMX_PES_OTHER works for me
+	 */
+	pes_type=DMX_PES_OTHER;
 	pes_filter.pes_type = pes_type;
 
 	if(ioctl(fd, DMX_SET_PES_FILTER, &pes_filter) < 0)
 	{
 		error("ioctl DMX_SET_PES_FILTER: %s", strerror(errno));
+error("pid=%d type=%d", pid, pes_type);
 		close(fd);
 		return -1;
 	}
