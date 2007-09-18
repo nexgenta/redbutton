f03d3cdcdae2d53cdfc27a451e48a3c9fd2f559b
skilvington <skilvington@79acd270-d80d-0410-8ebe-ae0fbb1cb39d>
2007-09-18 12:12:11 +0000

    convert STRINGs to OctetStrings
    git-svn-id: https://redbutton.svn.sourceforge.net/svnroot/redbutton/redbutton-author/trunk@391 79acd270-d80d-0410-8ebe-ae0fbb1cb39d

--- der_encode.c
+++ der_encode.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "der_encode.h"
 #include "utils.h"
 
 void
@@ -35,11 +36,77 @@ der_encode_OctetString(unsigned char **out, unsigned int *len, const char *str)
 		fatal("der_encode_OctetString: length already %u", *len);
 
 	/* convert the STRING, QPRINTABLE or BASE64 data to an OctetString */
-/* TODO: fixme */
-*len = strlen(str) + 1;
-*out=safe_malloc(*len);
-strcpy(*out,str);
+	if(*str == '"')
+		convert_STRING(out, len, str);
+	else if(*str == '\'')
+		convert_QPRINTABLE(out, len, str);
+	else if(*str == '`')
+		convert_BASE64(out, len, str);
+	else
+		fatal("der_encode_OctetString: str[0]=0x%02x", *str);
 
 	return;
+}
+
+/*
+ * string is enclosed in "
+ * " and \ within the string are encoded as \" and \\
+ */
+
+void
+convert_STRING(unsigned char **out, unsigned int *len, const char *str)
+{
+	unsigned char *p;
+
+	/* max size it could be */
+	*out = safe_malloc(strlen(str));
+
+	/* skip the initial " */
+	str ++;
+	p = *out;
+	while(*str != '"')
+	{
+		if(*str != '\\')
+		{
+			*p = *str;
+			p ++;
+			str ++;
+		}
+		else if(*(str + 1) == '"')
+		{
+			*p = '"';
+			p ++;
+			str += 2;
+		}
+		else if(*(str + 1) == '\\')
+		{
+			*p = '\\';
+			p ++;
+			str += 2;
+		}
+		else
+		{
+			fatal("Invalid escape sequence in STRING: %s", str - 1);
+		}
+	}
+
+	/* check we got to the closing quote */
+	if(*(str + 1) != '\0')
+		fatal("Unquoted \" in STRING: %s", str - 1);
+
+	/* return the length (note: no \0 terminator) */
+	*len = (p - *out);
+
+	return;
+}
+
+void
+convert_QPRINTABLE(unsigned char **out, unsigned int *len, const char *str)
+{
+}
+
+void
+convert_BASE64(unsigned char **out, unsigned int *len, const char *str)
+{
 }
 
--- der_encode.h
+++ der_encode.h
@@ -11,5 +11,9 @@ void der_encode_BOOLEAN(unsigned char **, unsigned int *, bool);
 void der_encode_INTEGER(unsigned char **, unsigned int *, int);
 void der_encode_OctetString(unsigned char **, unsigned int *, const char *);
 
+void convert_STRING(unsigned char **, unsigned int *, const char *);
+void convert_QPRINTABLE(unsigned char **, unsigned int *, const char *);
+void convert_BASE64(unsigned char **, unsigned int *, const char *);
+
 #endif	/* __DER_ENCODE_H__ */
 
