64e5e7d1d5b140e917bcbfbb2e93d2b1bacd92d0
skilvington <skilvington@79acd270-d80d-0410-8ebe-ae0fbb1cb39d>
2008-09-03 19:57:53 +0000

    typo
    git-svn-id: https://redbutton.svn.sourceforge.net/svnroot/redbutton/redbutton-browser/trunk@502 79acd270-d80d-0410-8ebe-ae0fbb1cb39d

--- MHEGApp.c
+++ MHEGApp.c
@@ -108,7 +108,7 @@ MHEGApp_loadScene(MHEGApp *m, OctetString *derfile)
 
 	/* assert */
 	if(derfile->size < 3 || strncmp(derfile->data, "~//", 3) != 0)
-		fatal("MHEGApp_loadApplication: group ID '%.*s' is not absolute", derfile->size, derfile->data);
+		fatal("MHEGApp_loadScene: group ID '%.*s' is not absolute", derfile->size, derfile->data);
 
 	if(m->scene != NULL)
 		free_InterchangedObject(m->scene);
